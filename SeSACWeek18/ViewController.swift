7f71aae7dea162936e083eb3aaadae53ef63ee9c
YongSeok Lee <paninology@gmail.com>
2022-11-02 11:14:31 +0900

    Initial Commit

---
+++ SeSACWeek18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek18
+//
+//  Created by yongseok lee on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
